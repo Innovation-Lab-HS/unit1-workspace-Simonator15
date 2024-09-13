824d0dc86de79be08e5e1f0a7764290fd4ff529e
Simonator15 <1108842@apps.nsd.org>
2024-09-13 18:21:15 +0000

    Stuff

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+  // all code in java sits inside a class
+
+  // every java program needs a public main metheod
+  // for the next while, ALL code you write will live inside main
+  public static void main(String[] args) {
+  System.out.println("Hello World");
+  }
+}

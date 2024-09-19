4f9d33de13c91ecdacd268ce952b982f75d20ca6
Simonator15 <1108842@apps.nsd.org>
2024-09-19 17:38:07 +0000

    Turtle Stuff

---
+++ src/main/Something.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Something {
+  public static void main(String[] args) {
+    // Double Forward Slash Means Comment
+    System.out.println();
+    System.out.println("I'm going to kill you with fire :D. ");
+    System.out.println();
+  }
+}

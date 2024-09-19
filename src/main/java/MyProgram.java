2f2e4ae0b1b632af5ef909aa7fd8f74c8032507e
Simonator15 <1108842@apps.nsd.org>
2024-09-19 18:04:41 +0000

    Added Stuff

---
+++ src/main/java/MyProgram.java
@@ -0,0 +1,7 @@
+public MyProgram {
+    public static void main(String[] args) {
+        System.out.println("This is a test of the")
+        System.out.Println("emergency broadcast system.");
+    }
+}
+
